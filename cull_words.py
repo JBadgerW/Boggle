79c9d02f8cc0f59616ecf1e0a654f48a53576f93
JBadgerW <128995045+JBadgerW@users.noreply.github.com>
2025-01-20 12:09:26 -0500

    Changed cull_words.py to comment out any word lists of truncated words.

--- cull_words.py
+++ cull_words.py
@@ -10,21 +10,20 @@ for num_letters in range(3, 17):
 
 for key in word_lists:
     filename = f"words_{key}_letters.txt"
-    filename_of_trunc = f"truncated_words_{key}_letters.txt"
+    #filename_of_trunc = f"truncated_words_{key}_letters.txt"
 
     with open(filename, "w") as words_file:
         word_list = list(word_lists[key])
         word_list.sort()
         words_file.writelines(word_list)
     
-    with open(filename_of_trunc, "w") as truncated_words_file:
-        word_list = list(word_lists[key])
-        word_list = {word[0:3] + "\n" for word in word_list}
-        word_list = list(word_list)
-        word_list.sort()
-        #print(word_list)
-        #breakpoint()
-        truncated_words_file.writelines(word_list)
+    # with open(filename_of_trunc, "w") as truncated_words_file:
+    #     word_list = list(word_lists[key])
+    #     word_list = {word[0:3] + "\n" for word in word_list}
+    #     word_list = list(word_list)
+    #     word_list.sort()
+
+    #     truncated_words_file.writelines(word_list)
 
 
 
